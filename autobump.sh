654f63434f0a1dc5a04dc1482a519ea5fe223351
Cyril AKNINE <caknine@clever-age.com>
2025-02-05 23:25:43 +0100

    Updated upstream.

--- autobump.sh
+++ autobump.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-NGINX_DEB_VERSION=`curl -s https://nginx.org/packages/mainline/debian/pool/nginx/n/nginx/ |grep '"nginx_' | sed -n "s/^.*\">nginx_\(.*\)\~.*$/\1/p" |sort -Vr |head -1| cut -d'-' -f1`
+NGINX_DEB_VERSION=`curl -s https://nginx.org/packages/mainline/debian/pool/nginx/n/nginx/ |grep '"nginx_' | sed -n "s/^.*\">nginx_\(.*\)\~.*$/\1/p" |sort -Vr |head -1`
 DAV_VERSION=`curl -s https://api.github.com/repos/arut/nginx-dav-ext-module/tags |grep "name" |head -1 |sed -n "s/^.*v\(.*\)\".*$/\1/p"`
 
 docker build --pull -t deb-dch -f Dockerfile-deb-dch .
 docker run -it -v $PWD:/local -e HOME=/local deb-dch bash -c "cd /local && \
-    dch -b -M -v ${DAV_VERSION}+nginx-${NGINX_DEB_VERSION}~bullseye --distribution 'bullseye' 'Updated upstream.'"
+    dch -M -v ${DAV_VERSION}+nginx-${NGINX_DEB_VERSION}~bullseye --distribution 'bullseye' 'Updated upstream.'"
 
 git add debian/changelog
 git commit -m "Updated upstream."
